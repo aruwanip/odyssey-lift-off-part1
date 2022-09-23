8ce5ee923104658128c258e598102bef52843a7b
Pinkie Aruwani <aruwanip@gmail.com>
2022-09-23 17:13:15 -0400

    Server: Define query

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,11 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
+    type Query {
+        "Query to get tracks array for the homepage grid"
+        tracksForHome: [Track!]!
+    }
+    
     "A track is a group of Modules that teaches about a specific topic"
     type Track {
         id: ID!
