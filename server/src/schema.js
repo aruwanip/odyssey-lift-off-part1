400a3bed6866f24b808273f2181ea2d5f8c0633e
Pinkie Aruwani <aruwanip@gmail.com>
2022-09-23 17:11:47 -0400

    Server: Add graphql and apollo dependencies and define schema

---
+++ server/src/schema.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+    "A track is a group of Modules that teaches about a specific topic"
+    type Track {
+        id: ID!
+        "Track's title"
+        title: String!
+        "Track's main author"
+        author: Author!
+        "Track's main illustration to display in track card or track page detail"
+        thumbnail: String
+        "Track's approximate length to complete, in minutes"
+        length: Int
+        "Number of modules this track contains"
+        modulesCount: Int
+    }
+    
+    "Author of a complete Track"
+    type Author {
+        id: ID!
+        "Author's first and last name"
+        name: String!
+        "Author's profile picture URL"
+        photo: String
+    }
+`;
+
+module.exports = typeDefs;
